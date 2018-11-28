11985fc03fb6dc9839c4e34cd4c72e50d8235ab0
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2018-11-28 22:39:48 +0100

    Used click event and structural directivas nfIf and ngFor

--- 01-hello-world/src/app/components/body/body.component.ts
+++ 01-hello-world/src/app/components/body/body.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './body.component.html',
 })
 export class BodyComponent {
+  sentence: any = {
+    message: 'A great power carries a great responsibility',
+    author: 'Ben Parker'
+  };
+
+  show = true;
+
+  characters: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
 }
