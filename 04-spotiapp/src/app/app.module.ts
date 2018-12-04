3c617f4f85f41e26ca81de569fe2ed4dc8cef589
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2018-12-04 23:51:52 +0100

    Generated principal components (home, searcher, artist) and installed Bootstrap4 via CDN

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearcherComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
