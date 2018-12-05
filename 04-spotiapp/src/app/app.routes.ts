454813d6aac71239ba95e188542acfcfd88ff3e5
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2018-12-05 19:54:41 +0100

    Imported HttpClientModule to do http requests

--- 04-spotiapp/src/app/app.routes.ts
+++ 04-spotiapp/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearcherComponent } from './components/searcher/searcher.component';
 
 export const ROUTES: Routes = [
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
     {path: 'search', component: SearcherComponent},
     {path: '', pathMatch: 'full', redirectTo: 'home'},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
