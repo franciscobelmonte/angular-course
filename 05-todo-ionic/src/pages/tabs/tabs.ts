b74511b712c9a5b3106bff51c3298e357114d202
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2018-12-07 17:53:07 +0100

    Created CompletedPage and PendingPage and changed tabs to load each page

--- 05-todo-ionic/src/pages/tabs/tabs.ts
+++ 05-todo-ionic/src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PendingPage } from '../pending/pending.component';
+import { CompletedPage } from '../completed/completed.component';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = PendingPage;
+  tab2Root = CompletedPage;
 
   constructor() {
 
