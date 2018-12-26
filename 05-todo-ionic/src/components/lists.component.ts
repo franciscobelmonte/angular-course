c3af51b4690a9d2b15c129ed031f3b386bb1fc45
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2018-12-27 00:02:24 +0100

    Edit list when slide list to left

--- 05-todo-ionic/src/components/lists.component.ts
+++ 05-todo-ionic/src/components/lists.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { NavController, AlertController, ItemSliding } from "ionic-angular";
 import { TodoService } from "../services/todo.service";
 import { TodoList } from '../models/todo-list.model';
 import { AddListPage } from '../pages/add-list/add-list.component';
@@ -14,7 +14,8 @@ export class ListsComponent {
 
     constructor(
         public todoService: TodoService,
-        private navCtrl: NavController
+        private navCtrl: NavController,
+        private alertCtrl: AlertController
     ) {
 
     }
@@ -28,6 +29,33 @@ export class ListsComponent {
 
     deleteList(list: TodoList) {
         this.todoService.deleteList(list);
+    }
+
+    editList(list: TodoList, slidingItem: ItemSliding) {
+        slidingItem.close();
+        const alert = this.alertCtrl.create({
+            title: 'Edit name list',
+            message: 'Edit name list',
+            inputs: [{
+                name: 'title',
+                placeholder: 'List name...',
+                value: list.title
+            }],
+            buttons: [{
+                text: 'Cancel',
+            }, {
+                text: 'Save',
+                handler: form => {
+                    if (form.title.length === 0) {
+                        return;
+                    }
+                    list.title = form.title;
+                    this.todoService.saveListsToStorage();
+                }
+            }]
+        });
+
+        alert.present();
     }
 }
 
