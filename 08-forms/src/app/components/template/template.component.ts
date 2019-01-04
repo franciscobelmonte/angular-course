9c78de9c2f4e614966cc0d9c7598c06b58d3e235
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-01-04 17:40:50 +0100

    Used ngModel to bind inputs to variables in components

--- 08-forms/src/app/components/template/template.component.ts
+++ 08-forms/src/app/components/template/template.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
 export class TemplateComponent implements OnInit {
+  user: Object = {
+    name: 'Francisco',
+    surnames: 'Belmonte',
+    email: ''
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(form: NgForm) {
+    console.log('Saving form...');
+    console.log('Form', form);
+    console.log('Value', form.value);
+    console.log('User', this.user);
   }
 
 }
