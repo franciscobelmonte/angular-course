88d67173933d355e8bc96c2b834b1a545f904041
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-01-20 19:00:39 +0100

    Created separated module and import in root module

--- 12-maps/src/app/app.module.ts
+++ 12-maps/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,8 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
