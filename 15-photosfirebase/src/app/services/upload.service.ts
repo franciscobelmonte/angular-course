f825da3774c5d5ee965789a07a7233a843254b50
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-01 17:44:30 +0100

    Uploaded photos to firebase via UploadTask

--- 15-photosfirebase/src/app/services/upload.service.ts
+++ 15-photosfirebase/src/app/services/upload.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FileItem } from '../models/file-item';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,30 @@ export class UploadService {
   constructor(private db: AngularFirestore) { }
 
   uploadPhotos (photos: FileItem[]) {
-    console.log(photos);
+    const storageRef = firebase.storage().ref();
+
+    for (const photo of photos) {
+      photo.isUploading = true;
+      if (photo.progress >= 100) {
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${this.folder}/${photo.filename}`).put(photo.file);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, (snaphost: firebase.storage.UploadTaskSnapshot) => {
+       photo.progress = (snaphost.bytesTransferred / snaphost.totalBytes) * 100;
+      }, (error) => {
+        console.error(error);
+      }, () => {
+        console.log('Photo uploaded successfully');
+
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          photo.url = downloadURL;
+          photo.isUploading = false;
+          this.uploadPhoto({ name: photo.filename, url: photo.url });
+        });
+
+      });
+    }
   }
 
   uploadPhoto (photo: {name: string, url: string}) {
