947becfb6fd46fb5e5d3dae9b74a722b91ca4815
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-01 19:13:01 +0100

    Drag and drop countries using Angular CDK

--- 17-virtualscroll/src/app/app.module.ts
+++ 17-virtualscroll/src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
 import { DragComponent } from './components/drag/drag.component';
+import { CountriesComponent } from './components/countries/countries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
