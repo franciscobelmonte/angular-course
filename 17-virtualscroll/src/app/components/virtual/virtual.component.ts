53c2e79c9c66505e927cc088cdb5ab7a4a9190d2
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-01 19:00:00 +0100

    Go to position in virtual scroll

--- 17-virtualscroll/src/app/components/virtual/virtual.component.ts
+++ 17-virtualscroll/src/app/components/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -9,9 +10,23 @@ export class VirtualComponent implements OnInit {
 
   people = Array(500).fill(0);
 
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  gotoEnd () {
+    this.viewport.scrollToIndex(this.people.length);
+  }
+
+  gotoStart () {
+    this.viewport.scrollToIndex(0);
+  }
+
+  gotoMiddle () {
+    this.viewport.scrollToIndex(this.people.length / 2);
   }
 
 }
